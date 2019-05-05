8736db0209987d7b4edbebe278b9d507107513de
zhangyang <zhangyang@2345.com>
2019-05-05 17:20:17 +0800

    123

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('info')
+        $schedule->command('collect:info')
             ->timezone('Asia/Shanghai')
             ->everyMinute();
     }
