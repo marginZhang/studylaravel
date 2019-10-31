a1c76119af9ee5a8a230b9de034b5943802fd8da
zhangyang <zhangyang@2345.com>
2019-10-31 10:14:21 +0800

    fix

--- app/Console/Commands/Weibo.php
+++ app/Console/Commands/Weibo.php
@@ -8,14 +8,12 @@ class Weibo extends Command
 {
     /**
      * The name and signature of the console command.
-     *
      * @var string
      */
     protected $signature = 'weibo:get';
 
     /**
      * The console command description.
-     *
      * @var string
      */
     protected $description = 'Command description';
@@ -26,13 +24,12 @@ class Weibo extends Command
 
     protected $handle;
 
-    protected $cookies = 'ALF=1574918252; SCF=AtzR-kFibecdPAN1GiEw9uOnb8iVgbMQtuulZj7Qh6p5WgrJqa4H-71QnjQCeabUipjRYRKwnrA_FBr5MMfGT-Q.; SUB=_2A25ws5gRDeRhGeRP61oT8SbLzDiIHXVQXzhZrDV6PUJbktANLWSskW1NUFtyIwXoomM3WeopMCXEyvBiK1oVO_gv; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhXM0-x1s1cQRs_DY2429Qp5JpX5K-hUgL.FozpehnEeKnNS0B2dJLoI7L9dNHV9NxaqJ-t; SUHB=0mxlvnFI2dGNZ4; SSOLoginState=1572333633; _T_WM=73575964948; MLOGIN=1; WEIBOCN_FROM=1110106030; XSRF-TOKEN=f3d387; M_WEIBOCN_PARAMS=oid%3D4427429621516417%26luicode%3D10000011%26lfid%3D1005052108218774%26fid%3D1076032108218774%26uicode%3D10000011';
+    protected $cookies = 'ALF=1574918252; SCF=AtzR-kFibecdPAN1GiEw9uOnb8iVgbMQtuulZj7Qh6p5WgrJqa4H-71QnjQCeabUipjRYRKwnrA_FBr5MMfGT-Q.; SUB=_2A25ws5gRDeRhGeRP61oT8SbLzDiIHXVQXzhZrDV6PUJbktANLWSskW1NUFtyIwXoomM3WeopMCXEyvBiK1oVO_gv; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhXM0-x1s1cQRs_DY2429Qp5JpX5K-hUgL.FozpehnEeKnNS0B2dJLoI7L9dNHV9NxaqJ-t; SUHB=0mxlvnFI2dGNZ4; _T_WM=73575964948; MLOGIN=1; WEIBOCN_FROM=1110106030; M_WEIBOCN_PARAMS=luicode%3D10000011%26lfid%3D1005052108218774%26fid%3D1076032108218774%26uicode%3D10000011';
 
     public $st;
 
     /**
      * Create a new command instance.
-     *
      * @return void
      */
     public function __construct()
@@ -72,6 +69,7 @@ class Weibo extends Command
     public function rData()
     {
         $this->handle = fopen($this->dataDir, "r");
+
         return fread($this->handle, filesize($this->dataDir));
     }
 
@@ -89,57 +87,61 @@ class Weibo extends Command
 
     /**
      * Execute the console command.
-     *
      * @return void
      */
     public function handle()
     {
-        $text = $this->rData();
-        $url = 'https://m.weibo.cn/api/container/getIndex?containerid=1076031660963912';
+        $id = $this->rData();
+        $url  = 'https://m.weibo.cn/api/container/getIndex?containerid=1076031660963912';
 //        $url = 'https://m.weibo.cn/api/container/getIndex?containerid=1076032108218774';
-        $card = $this->getFirst($url);
-        if ($card['mblog']['text'] !== $text) {
-            $this->reply($card['mblog']['id']);
+        $newID = $this->getFirst($url);
+        if ($newID !== $id) {
+            $this->reply($newID);
         }
     }
 
-    private function getFirst($url) {
-        $res = $this->request->curl_request($url);
-        $res = json_decode($res, true);
-        $cards = $res['data']['cards'];
-        $result = array();
+    private function getFirst($url)
+    {
+        $res    = $this->request->curl_request($url);
+        $res    = json_decode($res, true);
+        $cards  = $res['data']['cards'];
+        $id = 0;
         foreach ($cards as $index => $card) {
             if ($card['card_type'] !== 9 || isset($card['mblog']['isTop'])) {
                 continue;
             }
-            $this->wData($card['mblog']['text']);
-            $result = $card;
+            echo $card['mblog']['text'] . "\n";
+            $id = $card['mblog']['id'];
+            $this->wData($id);
             break;
         }
 
-        return $result;
+        return $id;
     }
 
-    private function getCookies() {
+    private function getCookies()
+    {
         $url = 'https://m.weibo.cn/p/1005052108218774';
 
         $header = $this->request->curl_request($url, '', $this->cookies, 1);
 
         preg_match_all("/XSRF\-TOKEN\=([^;]*);/", $header['cookie'], $matches);
         $this->st = $matches[1][1];
+        $this->cookies .= "; XSRF-TOKEN=" . $this->st;
     }
 
-    private function reply($id) {
+    private function reply($id)
+    {
         $this->getCookies();
-        $url = 'https://m.weibo.cn/api/comments/create';
+        $url  = 'https://m.weibo.cn/api/comments/create';
         $post = array(
-            'id' => $id,
+            'id'      => $id,
             'content' => $this->getRandMsg(),
-            'mid' => $id,
-            'st' => $this->st,
+            'mid'     => $id,
+            'st'      => $this->st,
         );
-        $res = $this->request->curl_request($url, $post, $this->cookies);
-        $res = json_decode($res, true);
+        $res  = $this->request->curl_request($url, $post, $this->cookies);
+        $res  = json_decode($res, true);
         var_export($res['ok']);
     }
 }
